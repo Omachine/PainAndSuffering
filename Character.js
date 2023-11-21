9b335a054a178b1925e4a7716350bc811b41c26b
Francisco <franciscoliveira7777@gmail.com>
2023-11-21 21:58:31 +0000

    fixed stuff

--- Character.js
+++ Character.js
@@ -1,56 +1,50 @@
-let isGameRunning = true;
-let isPlayerMoving = false;
-let points = 0;
-var character = document.querySelector(".character");
+let isGameRunning = true
+let isPlayerMoving = false
+let points = 0
+let character = document.querySelector('.character')
+let x = 0
+let y = 0
 
-document.getElementById("pause").addEventListener("click", function () {
-  character.style.animationPlayState = "paused";
-});
+document.getElementById('pause').addEventListener('click', function () {
+    character.style.animationPlayState = 'paused'
+})
 
-document.getElementById("restart").addEventListener("click", function () {
-  window.location.href = "Start.html"; // Navigate to the start page
-});
+document.getElementById('restart').addEventListener('click', function () {
+    window.location.href = 'Start.html' // Navigate to the start page
+})
 
 //-----Player------
 
-class Player {
-  constructor() {
-    this.html = document.getElementById("character" + id);
-    this.x = 0;
-    this.y = 0;
-    this.IsFacingLeft = false;
-  }
-  goTo(direction) {
-    if (!this.checkPosition(direction) || !isGameRunning) return;
-  }
-  checkPosition(direction) {
-    return !(
-      this.x + direction[0] < 0 ||
-      this.x + direction[0] > 2 ||
-      this.y + direction[1] < 0 ||
-      this.y + direction[1] > 2
-    );
-  }
-}
-var character = document.getElementById("character");
-var step = 150; // The number of pixels the character moves in each step
+var step = 150 // The number of pixels the character moves in each step
 
-window.addEventListener("keydown", function (event) {
-  var left = character.offsetLeft; // Current left position
-  var top = character.offsetTop; // Current top position
+window.addEventListener('keydown', function (event) {
+    let left = character.offsetLeft // Current left position
+    let top = character.offsetTop // Current top positions
 
-  switch (event.key) {
-    case "ArrowLeft": // If the left arrow key is pressed
-      character.style.left = left - step + "px";
-      break;
-    case "ArrowRight": // If the right arrow key is pressed
-      character.style.left = left + step + "px";
-      break;
-    case "ArrowUp": // If the up arrow key is pressed
-      character.style.top = top - step + "px";
-      break;
-    case "ArrowDown": // If the down arrow key is pressed
-      character.style.top = top + step + "px";
-      break;
-  }
-});
+    switch (event.key) {
+        case 'ArrowLeft': // If the left arrow key is pressed
+            if (x != 0) {
+                x--
+                character.style.left = left - step + 'px'
+            }
+            break
+        case 'ArrowRight': // If the right arrow key is pressed
+            if (x != 2) {
+                x++
+                character.style.left = left + step + 'px'
+            }
+            break
+        case 'ArrowUp': // If the up arrow key is pressed
+            if (y != 0) {
+                y--
+                character.style.top = top - step + 'px'
+            }
+            break
+        case 'ArrowDown': // If the down arrow key is pressed
+            if (y != 2) {
+                y++
+                character.style.top = top + step + 'px'
+            }
+            break
+    }
+})
